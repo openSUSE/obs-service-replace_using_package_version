3457a2249549782539e9a57d103c544257cc0396
David Cassany <dcassany@suse.com>
2018-03-20 16:58:54 +0100

    Remove makefile

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 
 import os
 import platform
+import re
 
 from setuptools import setup
 from distutils.command import install as distutils_install
 from distutils.command.sdist import sdist as distutils_sdist
 
-from replaceUsingPackageVersion.version import __version__
-
+def get_version_from_spec():
+    pattern = re.compile('Version: *(.*)$')
+    with open('obs-service-replace_using_package_version.spec', 'r') as spec:
+        for line in spec.readlines():
+            match = pattern.search(line)
+            if match:
+                return match.group(1)
+            
 
 class install(distutils_install.install):
     """
@@ -26,13 +33,13 @@ class install(distutils_install.install):
 
 setup(
     name = "replace_using_package_version",
-    version = __version__,
+    version = get_version_from_spec(),
     author = "David Cassany",
     author_email = "dcassany@suse.com",
     description = "Replaces a regex  with the version value of a package",
     license = "GPL",
     keywords = "open build service",
-    url = "https://github.com/davidcassany/obs-service-replace_using_package_version",
+    url = "https://github.com/openSUSE/obs-service-replace_using_package_version",
     packages=['replaceUsingPackageVersion'],
     classifiers=[
         'Intended Audience :: Developers',
