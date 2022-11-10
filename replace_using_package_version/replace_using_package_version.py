openSUSE/obs-service-replace_using_package_version

commit 43e56dd1f8e159c26964b2d48e87a0c81533e008

Switch from custom run_command to using check_output

replace_using_package_version/replace_using_package_version.py
@@ -45,6 +45,7 @@ import re
 import os
 import subprocess
 from pkg_resources import parse_version
+from typing import List
 
 version_regex = {
     'major': r'^(\d+)',
@@ -137,24 +138,8 @@ def find_match_in_version(regexpr, version):
         return search.group(1)
 
 
-def run_command(command):
-    process = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        env=os.environ
-    )
-    output, error = process.communicate()
-    if process.returncode != 0:
-        if not error:
-            error = bytes(b'(no output on stderr)')
-        if not output:
-            output = bytes(b'(no output on stdout)')
-        raise Exception((
-            'Command "{0}" failed\n\tstdout: {1}\n'
-            '\tstderr: {2}'
-        ).format(' '.join(command), output.decode(), error.decode()))
-    return output.decode()
+def run_command(command: List[str]) -> str:
+    return subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
 
 
 def get_pkg_name_from_rpm(rpm_file):

test/replace_using_package_version_test.py
@@ -1,5 +1,5 @@
 import sys
-from mock import patch, mock_open, call, Mock
+from mock import patch, mock_open, call
 
 from replace_using_package_version.replace_using_package_version import (
     apply_regex_to_file,
@@ -316,24 +316,10 @@ class TestRegexReplacePackageVersion(object):
         except Exception as e:
             assert 'Output directory outdir not found' in str(e)
 
-    @patch('subprocess.Popen')
-    def test_run_command(self, mock_subprocess):
-        mock_process = Mock()
-        mock_process.communicate.return_value = (b'stdout', b'stderr')
-        mock_process.returncode = 0
-        mock_subprocess.return_value = mock_process
+    @patch('subprocess.check_output')
+    def test_run_command(self, mock_check_output):
+        mock_check_output.return_value = b'stdout'
         assert run_command(['/bin/dummycmd', 'arg1']) == 'stdout'
-
-    @patch('subprocess.Popen')
-    def test_run_command_failure(self, mock_subprocess):
-        mock_process = Mock()
-        mock_process.communicate.return_value = (None, None)
-        mock_process.returncode = 1
-        mock_subprocess.return_value = mock_process
-        try:
-            run_command(['dummycmd', 'arg1']) == 'stdout'
-        except Exception as e:
-            assert 'Command "dummycmd arg1" failed' in str(e)
 
     @patch((
         'replace_using_package_version.'

commit 01a4d6a7ff7cf6a2a15fa2e19592b1c4d9d72bbc

Ignore stderr on successful command executions
This fixes a regression from 43e56dd1. On success, the command output
should only include the stdout, otherwise we get the normalized output
of rpm queries polluted with potential warning messages.

In case of error python's subprocess will already rise an Exception
with stdout, sdterr and return code.

Signed-off-by: David Cassany <dcassany@suse.com>

replace_using_package_version/replace_using_package_version.py
@@ -189,7 +189,7 @@ def find_match_in_version(regexpr, version):
 
 
 def run_command(command: List[str]) -> str:
-    return subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
+    return subprocess.check_output(command).decode()
 
 
 def get_pkg_name_from_rpm(rpm_file):